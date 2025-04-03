6ca2e8c13c6a792fb0acb04303b5c75d8d95bb9f
AliAmouz <amouzali60@gmail.com>
2025-04-03 04:40:32 +0000

    modify the name and optimize the assets

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,18 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Amouz Ali </span>
+            from <span className="purple"> Agadir, Morocco.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+           
+            Currently studying computer science at ensias (ecole national sup de l'info et d'analyses des systemes).
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing video games
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
